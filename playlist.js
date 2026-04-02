899f4ba54898d60d82b2f2cad2d036f2750526d9
Carliel Sousa <carlielsousa10@hotmail.com>
2026-04-02 13:53:52 -0300

    Adicionado Timer, Stop-at-End e correção de nomes de arquivos

--- playlist.js
+++ playlist.js
@@ -1,18 +1,18 @@
 // Este é o seu novo "Banco de Dados" manual
 export const minhasMusicas = [
     {
-        titulo: "Sempre Que me Olhares (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
-        url: "media/musicas/Sempre Que me Olhares (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
-        capa: "media/capas/🔥 Free download Christian Cross Wallpaper For iPhone An Amazing on WallpaperSafari.jpg"
+        titulo: "Turn on Console insights in Settings to receive AI assistance for understanding and addressing console warnings and errors. Learn more.mp3",
+        url: "media/musicas/Sempre Que me Olhares (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3", // Use o novo nome simples aqui
+        capa: "media/capas/capa1.jpg"
     },
     {
         titulo: "O Zelo da Tua Casa (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
         url: "media/musicas/O Zelo da Tua Casa (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
-        capa: "media/capas/🔥 Free download Christian Cross Wallpaper For iPhone An Amazing on WallpaperSafari.jpg"
+        capa: "media/capas/capa1.jpg"
     },
     {
         titulo: "En Su Nombre (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
         url: "media/musicas/En Su Nombre (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
-        capa: "media/capas/🔥 Free download Christian Cross Wallpaper For iPhone An Amazing on WallpaperSafari.jpg"
+        capa: "media/capas/capa1.jpg"
     }
 ];
