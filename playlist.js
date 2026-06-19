08c44156eb40351c82c6b2d5deacf6de5ee01714
Carliel Sousa <carlielsousa10@hotmail.com>
2026-06-19 15:45:18 -0300

    Fix: Atualizada toda a playlist com caminhos curtos sem espaços

--- playlist.js
+++ playlist.js
@@ -1,33 +1,32 @@
-// Este é o seu novo "Banco de Dados" manual
 export const minhasMusicas = [
     {
         titulo: "Sempre Que Me Olhares",
-        artista: "Americas", // Adicione esta linha
-        url: "Media/Musicas/sempre-que-me-olhares.mp3",
-        capa: "Media/Capas/capa1.jpg"
-    },
-    {
-        titulo: "O Zelo da Tua Casa (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
-        artista: "Americas", // Adicione esta linha
-        url: "Media/musicas/O Zelo da Tua Casa (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
+        artista: "Americas / Som do Reino",
+        url: "Media/musicas/sempre-que-me-olhares.mp3",
         capa: "Media/capas/capa1.jpg"
     },
     {
-        titulo: "En Su Nombre (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
-        artista: "Americas _ Som do Reino", // Adicione esta linha
-        url: "Media/musicas/En Su Nombre (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
+        titulo: "O Zelo da Tua Casa",
+        artista: "Americas / Som do Reino",
+        url: "Media/musicas/zelo-da-tua-casa.mp3",
+        capa: "Media/capas/capa1.jpg" // Se for a mesma capa, repita o caminho correto
+    },
+    {
+        titulo: "En Su Nombre",
+        artista: "Americas / Som do Reino",
+        url: "Media/musicas/en-su-nombre.mp3",
         capa: "Media/capas/capa1.jpg"
     },
     {
-        titulo: "Ao Que Está Assentado Sobre o Trono (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
-        artista: "Americas _ Som do Reino", // Adicione esta linha
-        url: "Media/musicas/Ao Que Está Assentado Sobre o Trono (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
+        titulo: "Ao Que Está Assentado Sobre o Trono",
+        artista: "Americas / Som do Reino",
+        url: "Media/musicas/ao-que-esta-assentado.mp3",
         capa: "Media/capas/capa1.jpg"
     },
     {
-        titulo: "Felipe Rodrigues - Tudo é Perda (Ao Vivo)(M4A_128K).mp3",
-        artista: "Felipe Rodrigues", // Adicione esta linha
-        url: "Media/musicas/Felipe Rodrigues - Tudo é Perda (Ao Vivo)(M4A_128K).mp3",
+        titulo: "Tudo é Perda",
+        artista: "Felipe Rodrigues",
+        url: "Media/musicas/tudo-e-perda.mp3",
         capa: "Media/capas/capa1.jpg"
     }
 ];
