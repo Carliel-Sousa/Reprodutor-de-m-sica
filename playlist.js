f8560ce5ef854a58899acf80b7a9da377311c6dd
Carliel Sousa <carlielsousa10@hotmail.com>
2026-06-19 16:27:55 -0300

    Fix: Padronizando urls da playlist para nomes curtos

--- playlist.js
+++ playlist.js
@@ -9,7 +9,7 @@ export const minhasMusicas = [
         titulo: "O Zelo da Tua Casa",
         artista: "Americas / Som do Reino",
         url: "Media/musicas/zelo-da-tua-casa.mp3",
-        capa: "Media/capas/capa1.jpg" // Se for a mesma capa, repita o caminho correto
+        capa: "Media/capas/capa1.jpg"
     },
     {
         titulo: "En Su Nombre",
@@ -17,17 +17,16 @@ export const minhasMusicas = [
         url: "Media/musicas/en-su-nombre.mp3",
         capa: "Media/capas/capa1.jpg"
     },
-    // ... (suas outras músicas anteriores)
     {
         titulo: "Ao Que Está Assentado Sobre o Trono",
         artista: "Americas / Som do Reino",
-        url: "Media/musicas/Ao Que Está Assentado Sobre o Trono (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
+        url: "Media/musicas/ao-que-esta-assentado.mp3",
         capa: "Media/capas/capa1.jpg"
     },
     {
         titulo: "Tudo é Perda",
         artista: "Felipe Rodrigues",
-        url: "Media/musicas/Felipe Rodrigues - Tudo é Perda (Ao Vivo)(M4A_128K).mp3",
+        url: "Media/musicas/tudo-e-perda.mp3",
         capa: "Media/capas/capa1.jpg"
     }
 ];
