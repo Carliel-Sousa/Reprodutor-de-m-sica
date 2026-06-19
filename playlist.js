f7ad78a783867a3439731a7cf51fbdf4fc93b871
Carliel Sousa <carlielsousa10@hotmail.com>
2026-06-19 15:08:29 -0300

    Fix: Corrigindo caminhos 404 das músicas e capas

--- playlist.js
+++ playlist.js
@@ -3,31 +3,31 @@ export const minhasMusicas = [
     {
         titulo: "Sempre Que me Olhares (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
         artista: "Americas", // Adicione esta linha
-        url: "media/musicas/Sempre Que me Olhares (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3", // Use o novo nome simples aqui
-        capa: "media/capas/capa1.jpg"
+        url: "Media/musicas/Sempre Que me Olhares (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3", // Use o novo nome simples aqui
+        capa: "Media/capas/capa1.jpg"
     },
     {
         titulo: "O Zelo da Tua Casa (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
         artista: "Americas", // Adicione esta linha
-        url: "media/musicas/O Zelo da Tua Casa (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
-        capa: "media/capas/capa1.jpg"
+        url: "Media/musicas/O Zelo da Tua Casa (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
+        capa: "Media/capas/capa1.jpg"
     },
     {
         titulo: "En Su Nombre (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
         artista: "Americas _ Som do Reino", // Adicione esta linha
-        url: "media/musicas/En Su Nombre (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
-        capa: "media/capas/capa1.jpg"
+        url: "Media/musicas/En Su Nombre (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
+        capa: "Media/capas/capa1.jpg"
     },
     {
         titulo: "Ao Que Está Assentado Sobre o Trono (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
         artista: "Americas _ Som do Reino", // Adicione esta linha
-        url: "media/musicas/Ao Que Está Assentado Sobre o Trono (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
-        capa: "media/capas/capa1.jpg"
+        url: "Media/musicas/Ao Que Está Assentado Sobre o Trono (Clip Oficial) - Americas _ Som do Reino   TOMATULUGAR(M4A_128K).mp3",
+        capa: "Media/capas/capa1.jpg"
     },
     {
         titulo: "Felipe Rodrigues - Tudo é Perda (Ao Vivo)(M4A_128K).mp3",
         artista: "Felipe Rodrigues", // Adicione esta linha
-        url: "media/musicas/Felipe Rodrigues - Tudo é Perda (Ao Vivo)(M4A_128K).mp3",
-        capa: "media/capas/capa1.jpg"
+        url: "Media/musicas/Felipe Rodrigues - Tudo é Perda (Ao Vivo)(M4A_128K).mp3",
+        capa: "Media/capas/capa1.jpg"
     }
 ];
